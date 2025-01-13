90289f2477b09c9155e6a2223219bdd177432371
Maris Hume <marishume@gmail.com>
2025-01-13 15:25:38 -0400

    Test

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace WebApplication1.Models
 {
     public class ErrorViewModel
     {
+        //a model
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
